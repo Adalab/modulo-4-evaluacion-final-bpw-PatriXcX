517b9a260bfc5161efb296757eaaa4388f91fe91
PatriXcX <patriciapicogarcia@gmail.com>
2024-09-28 00:37:53 +0200

    Files modified to use node, express and work with server

---
+++ SRC/index.js
@@ -0,0 +1,94 @@
+// IMPORTAR BIBLIOTECAS
+
+const express = require("express");
+const cors = require("cors");
+
+const mysql = require("mysql2/promise");
+
+// CONEXIÓN A MYSQL
+const getConnection = require("./db/db");
+
+// CREAR VARIABLES
+const app = express(); // server
+const port = 4000;
+
+// CONFIGURACIÓN DE EXPRESS
+app.use(cors());
+app.use(express.json({ limit: "25mb" }));
+
+// prueba getConnection
+
+// ARRANCAR EL SERVIDOR
+
+app.listen(port, () => {
+  console.log(`Servidor iniciado escuchando en http://localhost:${port}`);
+});
+
+// ENDPOINTS API
+
+// maga = {id:1, nombre:"", expecialidad:"", lealtad:"", maestria:"", escuela_de_magia:""}
+// GET    /api/magas                 --> [{}, {}]
+// GET    /api/magas?search=Nombre       --> [{},{}]
+// GET    /api/magas?order=nombre        --> [{},{}]
+// GET    /api/magas?incluir_varita=true --> [{},{}]
+
+app.get("/api/music", async (req, res) => {
+  // Obtener conn con MySQL
+  const conn = await getConnection();
+
+  if (!conn) {
+    res.status(500).json({ success: false, error: "Error con la conexion." });
+
+    return;
+  }
+
+  // Lanzar una query
+
+  const [results] = await conn.query("SELECT * FROM bands;");
+
+  // Devolvemos los resultados como JSON
+
+  res.json(results);
+
+  // Cerrar la conn
+
+  await conn.close();
+});
+
+//Post insertar campos
+
+app.post("/api/music", async (req, res) => {
+  console.log(req.body);
+  //Obtener la conn
+  const conn = await getConnection();
+
+  if (!conn) {
+    res.status(500).json({ success: false, error: "Error con la conexion." });
+
+    return;
+  }
+
+  //Insert
+  const [results] = await conn.execute(
+    `Insert bands(Name,City,Genre,Albums,Active)
+ values (?,?,?,?,?);`,
+    [
+      req.body.Name,
+      req.body.City,
+      req.body.Genre,
+      req.body.Albums,
+      req.body.Active,
+    ]
+  );
+
+  //REspuest Json
+
+  if (results.affectedRows === 1) {
+    res.json({ success: true, id: results.insertId });
+  } else {
+    res.json({ success: false, error: "No insertado" });
+  }
+
+  //Cerrar conexión
+  await conn.close();
+});
